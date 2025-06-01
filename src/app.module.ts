9187db0dcc19c06e3963a3296b54c664a7c68180
Fenix-LAB <fenixcr.123@gmail.com>
2025-06-01 16:45:16 -0600

    feat: add AuthModule and integrate it into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TaskModule} from './task/task.module';
 import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TaskModule, ProjectsModule],
+  imports: [TaskModule, ProjectsModule, AuthModule],
 })
 export class AppModule {}
